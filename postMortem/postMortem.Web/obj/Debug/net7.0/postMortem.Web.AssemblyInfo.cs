38723cf7de075c4474f5a6861725ad49c969ee6e
austin <austindouglas@outlook.com>
2023-10-17 19:31:09 -0400

    Added comment looking for banned users

--- postMortem/postMortem.Web/obj/Debug/net7.0/postMortem.Web.AssemblyInfo.cs
+++ postMortem/postMortem.Web/obj/Debug/net7.0/postMortem.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("postMortem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8bd4ca89c126696406fae67da4c422c81c4c78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db6efe299ab7d0cfe0714c3414adb3c28c1aefc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("postMortem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("postMortem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
