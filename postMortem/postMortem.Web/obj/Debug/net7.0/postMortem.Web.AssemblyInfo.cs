f2e9e6714adfe72056ce48345166092776cfd5bd
austin <austindouglas@outlook.com>
2023-10-16 21:48:12 -0400

    Added settings to navmenu

--- postMortem/postMortem.Web/obj/Debug/net7.0/postMortem.Web.AssemblyInfo.cs
+++ postMortem/postMortem.Web/obj/Debug/net7.0/postMortem.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("postMortem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589fb81edc2247015e0c8ba01d2c2a8bfd98421f")]
 [assembly: System.Reflection.AssemblyProductAttribute("postMortem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("postMortem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
